d8259c933ce613a5799c954048909006ca13761f
Алексей Рыбак <rybak2108@yandex.ru>
2025-04-27 17:37:37 +0300

    checked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Главная страница</div>;
+  return (
+    <>
+      <div>Главная страница</div>
+      <Link href="posts/3">К посту</Link>
+    </>
+  );
 }
