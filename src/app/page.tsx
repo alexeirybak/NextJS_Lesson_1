3f1b8982a1f0c332df38f0d5ba2c33ba3cb876e7
Алексей Рыбак <rybak2108@yandex.ru>
2025-05-03 00:28:27 +0300

    checked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,3 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
-interface Comment {
-  id: string;
-  body: string;
-}
-
 export default function Home() {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    fetch(`/comments?query=${search}`)
-      .then((res) => res.json())
-      .then((data: Comment[]) => setComments(data))
-      .catch(() => setComments([]));
-  }, [search]);
-
-  return (
-    <>
-      <h1 className="text-xl font-bold mb-4">Комментарии</h1>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Поиск"
-        className="border-1"
-      />
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.id}z{comment.body}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <></>;
 }
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,56 @@
+// import { type NextRequest } from "next/server";
+
+// export async function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization"));
+//   return new Response("Данные пользователя");
+// }
+
+import { headers } from "next/headers";
+
 export async function GET() {
-  return new Response("Данные пользователя");
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  //return new Response("<h1>Данные пользователя</h1>");
+
+  return new Response("<h1>Данные пользователя</h1>", {
+    headers: {
+      "Content-Type": "text/html; charset=UTF-8",
+    },
+  });
 }
+
+// import { headers } from "next/headers";
+// export async function GET() {
+//   const headerList = headers();
+//   const acceptHeader = (await headerList).get("Accept");
+
+//   const userData = {
+//     name: "Иван Иванов",
+//     email: "ivan@example.com",
+//     role: "admin",
+//   };
+
+//   if (acceptHeader?.includes("application/json")) {
+//     return new Response(JSON.stringify(userData), {
+//       headers: {
+//         "Content-Type": "application/json; charset=UTF-8",
+//       },
+//     });
+//   }
+
+//   return new Response(
+//     `<h1>Профиль пользователя</h1>
+//     <ul>
+//     <li>Имя: ${userData.name}</li>
+//     <li>Email: ${userData.email}</li>
+//     <li>Роль: ${userData.role}</li>
+//     </ul>`,
+//     {
+//       headers: {
+//         "Content-Type": "text/html; charset=UTF-8",
+//       },
+//     }
+//   );
+// }
