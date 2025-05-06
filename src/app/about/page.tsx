1693d65dec7a48ee635642db41e3d5be96a3cc9e
Алексей Рыбак <rybak2108@yandex.ru>
2025-05-07 00:01:32 +0300

    checked

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function About() {
+import { cookies } from "next/headers";
+
+export default async function About() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   return (
     <>
       <h1>О компании</h1>
