b3fcf276fd93abf9b6295f556c3530d65c309d6d
Алексей Рыбак <rybak2108@yandex.ru>
2025-05-13 00:44:17 +0300

    checked

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -2,6 +2,7 @@
 
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import { updateProduct } from "@/prisma-db";
 
 export type Errors = {
   title?: string;
@@ -49,5 +50,43 @@ export async function createProduct(prevState: FormState, formData: FormData) {
   }
 
   await addProduct(title, parseInt(price), description);
+  redirect("/products-db");
+}
+
+export async function editProduct(
+  id: number,
+  prevState: FormState,
+  formData: FormData
+) {
+  const title = formData.get("title") as string;
+  const price = formData.get("price") as string;
+  const description = formData.get("description") as string;
+
+  const errors: Errors = {};
+
+  if (!title) {
+    errors.title = "Укажите название";
+  }
+
+  if (!price) {
+    errors.price = "Укажите цену";
+  }
+
+  if (!description) {
+    errors.description = "Укажите описание";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+      values: {
+        title,
+        price,
+        description,
+      },
+    };
+  }
+
+  await updateProduct(id, title, parseInt(price), description);
   redirect("/products-db");
 }
